7e0bacca6c29e6591d6ae6658153c925bf223175
Forhath-Hosain-Ome <omehasan3@gmail.com>
2025-07-13 23:14:22 +0600

    Depertment models done

--- Model_App/models/Subject_Model.py
+++ Model_App/models/Subject_Model.py
@@ -1,5 +1,7 @@
 from django.db import models
-from models import BaseModel
+from models import BaseModel, DepertmentModel
 
 class SubjectModel(BaseModel):
-    pass
+    title = models.CharField(max_length=30, unique=True)
+    subject_code = models.CharField(max_length=12, unique=True)
+    depertment = models.ForeignKey(DepertmentModel, on_delete=models.CASCADE)
