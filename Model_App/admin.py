065ca23179f4132e1ce7764dac3e54e03ffb9109
Forhath-Hosain-Ome <omehasan3@gmail.com>
2025-07-14 19:41:25 +0600

    model test complete working fine initialy

--- Model_App/admin.py
+++ Model_App/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from Model_App.models import AccountModel, CourseModel, DepertmentModel, SubjectModel
+from Model_App.models import AccountModel, CourseModel, DepertmentModel, SubjectModel, StudentEnrollment
 
 admin.site.register(AccountModel)
 admin.site.register(CourseModel)
 admin.site.register(DepertmentModel)
 admin.site.register(SubjectModel)
+admin.site.register(StudentEnrollment)
--- Model_App/models/__init__.py
+++ Model_App/models/__init__.py
@@ -4,3 +4,4 @@ from .Account_Model import AccountModel
 from .Course_Model import CourseModel
 from .Depertment_Model import DepertmentModel
 from .Subject_Model import SubjectModel
+from .Student_Enrollment import StudentEnrollment
