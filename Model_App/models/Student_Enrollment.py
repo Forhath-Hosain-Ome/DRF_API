ff7abe21a6d1cba5055edd569e636fb021eee02d
Forhath-Hosain-Ome <omehasan3@gmail.com>
2025-07-14 19:32:58 +0600

    models done testing now

--- Model_App/models/Student_Enrollment.py
+++ Model_App/models/Student_Enrollment.py
@@ -1,6 +1,17 @@
 from django.db import models
 from core.models import TimeStamp
-
+from Model_App.models import CourseModel, SubjectModel, AccountModel
 
 class StudentEnrollment(TimeStamp):
-    pass
+    Student = models.OneToOneField(AccountModel, on_delete=models.CASCADE)
+    course = models.ForeignKey(CourseModel, on_delete=models.CASCADE)
+    subjects = models.ForeignKey(SubjectModel, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'StudentEnrollment'
+        verbose_name = 'StudentEnrollment'
+        verbose_name_plural = 'StudentEnrollments'
+        ordering = ['Student', 'course', 'subjects']
+
+    def __str__(self):
+        return f'{self.Student} Is {self.course}'
