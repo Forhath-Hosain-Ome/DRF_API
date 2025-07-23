2ea32be0456b6f2bbae8a2ad3c9b7d4d98090a08
Forhath Hosain Ome <62540867+Forhath-Hosain-Ome@users.noreply.github.com>
2025-07-23 05:04:07 +0000

    Pending

--- root/urls.py
+++ root/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +11,8 @@ urlpatterns += [
     # path('obtain-auth-token/', obtain_auth_token),
     path('api-auth/', include('rest_framework.urls')),
     # path('accounts/profile/', lambda request: HttpResponse("Profile page")),
+]
+urlpatterns += [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
